678da43342a78636b02d62f11f123cd6da69f91f
Admin <Admin@ADMIN-PC>
2026-04-01 16:32:35 +0700

    buoii 1

--- nhapmonlaptrinh/buoi6/buoi1.5/buoi1/buoi1.cpp
+++ nhapmonlaptrinh/buoi6/buoi1.5/buoi1/buoi1.cpp
@@ -1,20 +1,76 @@
-// buoi1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+struct Person {
+    int id, age;
+    string name, hometown;
+};
+
+void Show(vector<Person>p) {
+    if (p.size() == 0)
+        cout << "An empty list" << endl;
+    else {
+        for (int i = p.size() - 1; i >= 0; i--) {
+            cout << "\t + Id: " << p[i].id << endl;
+            cout << "\t + name: " << p[i].name << endl;
+            cout << "\t + age: " << p[i].age << endl;
+            cout << "\t + Home town: " << p[i].hometown << endl;
+        }
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void main() {
+    do {
+        cout << "---- HUMAN RESOURCE MANAGEMENT ----" << endl;
+        cout << "1. Show person list" << endl;
+        cout << "2. Add a person" << endl;
+        cout << "3. Remove a person by id" << endl;
+        cout << "4. Find people by name" << endl;
+        cout << "5. Export to file" << endl;
+        cout << "6. Import from file" << endl;
+        cout << "_______________________" << endl;
+        cout << "your command: ";
+        int cmd;
+        cin >> cmd;
+        switch (cmd) {
+        case 1: {
+            Show(list);
+            break;
+        }
+        case 2: {
+            Person a;
+            cout << "Id: ";
+            cin >> a.id;
+            cout << "Name: ";
+            cin >> a.name;
+            cout << "Age: ";
+            cin >> a.age;
+            cout << "Home town: ";
+            cin >> a.hometown;
+            
+            break;
+        }
+        case 3: {
+            break;
+        }
+        case 4: {
+            break;
+        }
+        case 5: {
+            break;
+        }
+        case 6: {
+            break;
+        }
+        case 0: {
+            return;
+        }
+        default: {
+            cout << "your command isn`t found, try again" << endl;
+        }
+        }
+        cout << " press Enter to continue... ";
+        cin.ignore();
+        cin.get();
+        } while (true);
+    }
